d10b3b67af3cd52c8a26d1f3083ee59da361b2f8
DESKTOP-H4HTUE5\LIMSUJIN <resqpa79@gmail.com>
2022-06-25 11:35:32 +0900

    메인 화면 변경 (임시)

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Bbob
 bo n bbo study
+
+# start
+> npm install
+> npm start
+
+# history
+[2022-06-25] 메인 화면 변경 (임시)
--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
+import logo from './teletubbies.gif';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} alt="logo" />
+        <p>안녕 ~ 여기는 보와 뽀의 공부방이야 ~</p>
       </header>
     </div>
   );
